d6c4e7f2ddb2fcefadfdd995b40c18f452a112fd
joselemus <jose.webfreelance@gmail.com>
2018-07-04 13:12:05 -0600

    Cambios en contacto

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavigationServiceService} from '../../shared/services/navigation-service.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-contact',
@@ -10,17 +12,38 @@ export class ContactComponent implements OnInit, OnDestroy {
 
   subscription;
   nameClass;
+  contactForm: FormGroup;
 
-  constructor(private navigationService: NavigationServiceService) {
+  constructor(
+    private navigationService: NavigationServiceService,
+    private fb: FormBuilder,
+    title: Title
+  ) {
     this.subscription = navigationService.notify.subscribe(value => this.changeClass(value));
+    title.setTitle('Formulario Contacto');
+    this.buildForm();
   }
-
 
   ngOnInit() {
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  buildForm() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      subject: ['', Validators.compose([Validators.required])],
+      message: ['', Validators.compose([Validators.required])],
+      phone: ['', Validators.compose([Validators.maxLength(8)])]
+    });
+  }
+
+  onSubmit() {
+    const name = this.contactForm.get('name').value;
+    console.log(name)
   }
 
   changeClass(taxon) {
