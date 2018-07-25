4f501555839b46f1ab749f47ace64f530c5cc3c4
josewebfreelance <jose.webfreelance@gmail.com>
2018-07-25 00:08:05 -0600

    Cambios de disenio

--- src/app/home/content/welcome/welcome.component.ts
+++ src/app/home/content/welcome/welcome.component.ts
@@ -1,24 +1,27 @@
 import {Component, EventEmitter, OnDestroy, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {NavigationServiceService} from '../../../shared/services/navigation-service.service';
 import {Observable} from 'rxjs/Observable';
+import {taxonomiList} from '../nav-side/shared/taxonomi-list';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class WelcomeComponent implements OnInit, OnDestroy {
   @Output() navigate = new EventEmitter<number>();
 
   subscription;
   nameClass = 'fadeIn';
+  taxonList = taxonomiList;
+  route = '/contacto';
 
   constructor(private navigationService: NavigationServiceService) {
     this.subscription = navigationService.notify.subscribe(value => this.changeClass(value));
   }
 
   ngOnInit() {
+    this.taxonList = this.taxonList.filter(obj => obj.id === 4);
   }
 
   ngOnDestroy() {
@@ -39,6 +42,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
 
   navigatePosition(position) {
     this.navigate.emit(position);
+    console.log(position)
   }
 
 }
